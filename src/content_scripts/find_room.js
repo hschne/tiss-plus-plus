048e18a2e9e71975773b0b74c7b2667aa227232d
hschroedl <hansj.schroedl@gmail.com>
2015-07-09 17:08:29 +0200

    Add find room functions.

--- src/content_scripts/find_room.js
+++ src/content_scripts/find_room.js
@@ -1,4 +1,34 @@
 
-function addFindRoomButton(){
+function addFindRoomButtons(){
 
+}
+
+
+function createRoomLink(roomName){
+    var roomLink = $('<a>',{
+        text: false,
+        title: "Link to rooms page",
+        href: 'http://www.wegweiser.ac.at/tuwien/hoersaal/'+roomName+'.html'
+    });
+    return roomLink;
+}
+
+function getMapFromRoomName(roomName){
+
+
+}
+
+function retrieveRoomNameList(callback){
+
+}
+
+function addButtonToRoomLink(room_link, button){
+    var parentElement = room_link.parent();
+    parentElement.append(button)
+}
+
+function getRoomLinks(){
+    var matchingUrl = "/events/roomSchedule.xhtml";
+    var links = $("a[href*='"  +matchingUrl + "']");
+    return links;
 }
