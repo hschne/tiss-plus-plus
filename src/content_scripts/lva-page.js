4a3c202709dc28e3cf80fa3fdd7b1de7b62c5424
hschroedl <hansj.schroedl@gmail.com>
2015-08-30 20:56:29 +0200

    Implement links to wegweiser

--- src/content_scripts/lva-page.js
+++ src/content_scripts/lva-page.js
@@ -1,26 +1,38 @@
-function GetRoomLinks() {
-    var roomLinks = $("a[href^='/events/roomSchedule.xhtml']");
-    return roomLinks;
+
+function createButton(linkElement) {
+    var currentRoom = linkElement.innerHTML;
+
+    function renderButton(linkElement, buttonLink) {
+        var parent = linkElement.parentElement;
+        var parentContent = parent.innerHTML;
+        parent.innerHTML = parentContent + "<a href="+buttonLink+" >Map</a>"
+    }
+
+    function getRoomLinkAndRenderButton() {
+        var baseLink = "http://www.wegweiser.ac.at/";
+        $.getJSON(chrome.extension.getURL('/resources/room-links.json'), function (json) {
+            var rooms = json.rooms;
+            $.each(rooms, function (index, room) {
+                var roomName = room.name;
+                if (roomName == currentRoom) {
+                    var link = baseLink + room.link;
+                    renderButton(linkElement, link)
+                }
+            })
+        });
+    }
+    getRoomLinkAndRenderButton();
 }
 
-function createButton(){
-    var button = document.createElement("button"),
-        text = document.createTextNode("test");
-    button.appendChild(text);
-    return button;
+function renderAllRoomButtons(){
+
+    var roomLinks =$("a[href^= '/events/roomSchedule.xhtml']");
+    roomLinks.each(function () {
+        var link = $(this);
+        createButton(link.get(0));
+    })
 }
 
-var button = createButton();
-
-this.renderButton = function(link){
-    link.innerHTML = "FUNKY MONKY"
-}
+renderAllRoomButtons();
 
 
-var roomLinks = GetRoomLinks();
-
-roomLinks.each(function(index){
-    var link = $(this);
-    console.log(index + ": " +$(this).text());
-    renderButton(link.get(0));
-})
