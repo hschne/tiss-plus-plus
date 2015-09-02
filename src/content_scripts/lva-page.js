87e2e99a3f37c662792474e0ba692c201503af9c
hschroedl <hansj.schroedl@gmail.com>
2015-09-02 16:55:57 +0200

    Fix a bug with opening links, improve styling.

--- src/content_scripts/lva-page.js
+++ src/content_scripts/lva-page.js
@@ -3,20 +3,21 @@ function renderRoomButtons(){
     function createButton(linkElement) {
         var currentRoom = linkElement.innerHTML;
 
-        var buttonHtml = "<button class='mapbutton'>M</button>";
+        var buttonHtml = "<div class='mapbutton'></div>";
 
         function renderButton(linkElement, buttonLink) {
             var parent = linkElement.parentElement;
             var parentContent = parent.innerHTML;
-            parent.innerHTML = parentContent + "<a href="+buttonLink+" >" +buttonHtml+"</a>";
+
+            parent.innerHTML = parentContent +  '<a class=\"maplink\"href=\"'+buttonLink+ '\" >' +buttonHtml+ "</a>";
         }
 
         function getRoomLinkAndRenderButton() {
-            var baseLink = "http://www.wegweiser.ac.at/";
+            var baseLink = "http://www.wegweiser.ac.at";
             $.getJSON(chrome.extension.getURL('/resources/room-links.json'), function (json) {
                 var rooms = json;
                 $.each(rooms, function (index, room) {
-                    if (matchesCurrentRoom(room.name)) {
+                    if (room.name.indexOf(currentRoom) > -1) {
                         var link = baseLink + room.link;
                         renderButton(linkElement, link)
                     }
@@ -24,20 +25,10 @@ function renderRoomButtons(){
             });
         }
 
-        function matchesCurrentRoom(scrapedRoom) {
-            var formattedScrapedRoom = scrapedRoom.replace(" - TU Wien","");
-            if(currentRoom == "EI 7 Hörsaal"){
-                var i =5;
-            }
-            formattedScrapedRoom = formattedScrapedRoom.replace(/ +(?= )/g,"");
-            var formattedTissRoom = currentRoom.replace(" Hörsaal", "");
-            var contains = ((formattedScrapedRoom.indexOf(formattedTissRoom) > -1) || formattedScrapedRoom == formattedTissRoom);
-            return contains;
-        }
         getRoomLinkAndRenderButton();
     }
 
-    var roomLinks =$("a[href^= '/events/roomSchedule.xhtml']");
+    var roomLinks =$("a[href*='/events/roomSchedule.xhtml']");
     roomLinks.each(function () {
         var link = $(this);
         createButton(link.get(0));
