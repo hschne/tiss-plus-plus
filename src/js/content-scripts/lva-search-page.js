e4c12e6e0e97f871d4e361f19f2f0d224fd42819
hschroedl <hansj.schroedl@gmail.com>
2015-09-17 12:06:48 +0200

    Dont render content if a table already exists

--- src/js/content-scripts/lva-search-page.js
+++ src/js/content-scripts/lva-search-page.js
@@ -4,8 +4,19 @@
 function LvaSearchPage() {
 
     this.displayRecentlyVisitedLvas = function (table) {
-        var contentInner = $('#contentInner').get(0);
-        var existingContent = contentInner.innerHTML;
-        contentInner.innerHTML = existingContent + table;
+        /**
+         * @return {boolean}
+         */
+        function AnotherTableExists(){
+            var anotherTable = $("#courseList\\:courseTable");
+            return anotherTable.length != 0;
+
+        }
+
+        if(!AnotherTableExists()){
+            var contentInner = $('#contentInner').get(0);
+            var existingContent = contentInner.innerHTML;
+            contentInner.innerHTML = existingContent + table;
+        }
     }
 }
--- src/test/content-scripts/lva-search-page-tests.js
+++ src/test/content-scripts/lva-search-page-tests.js
@@ -16,3 +16,15 @@ QUnit.test('Render content in content-inner', function (assert) {
     var newContent = $('#renderedContent');
     assert.equal(newContent.text(), "Hello", "Content has been rendered");
 });
+
+QUnit.test('Another table already exists', function (assert) {
+    var pageContentMock = "<div id='contentInner'><div id='courseList:courseTable'></div></div>";
+    $('#qunit-fixture').append(pageContentMock);
+    var contentToAdd = "<div id='renderedContent'>Hello</div>"
+
+    var lvaSearchPage = new LvaSearchPage();
+    lvaSearchPage.displayRecentlyVisitedLvas(contentToAdd);
+
+    var newContent = $('#renderedContent');
+    assert.equal(newContent.length, 0, "No new content has been rendered");
+});
