01991131e37be0e8559cff205b00845c27574ef2
hschroedl <hansj.schroedl@gmail.com>
2015-09-22 21:31:09 +0200

    Implement insert of static date to calendar

--- src/js/calendar.js
+++ src/js/calendar.js
@@ -5,20 +5,39 @@ var calendar = (function() {
 
     var _identity;
 
-    var _CALENDAR_LIST_URI = "https://www.googleapis.com/calendar/v3/users/me/calendarList";
+    var _CALENDAR_EVENT_URI = "https://www.googleapis.com/calendar/v3/calendars/primary/events";
 
 
     var init = function(identity){
         _identity = identity;
     };
 
+    var _createEventData = function(eventData) {
+        var startTime = new Date('2015-09-30T09:00:00').toJSON();
+        var endTime = new Date('2015-09-30T09:05:00').toJSON();
+        var event = {
+            'summary': 'Anmeldung für LVA',
+            'start': {
+                'dateTime': startTime
+            },
+            'end': {
+                'dateTime': endTime
+            },
+            'reminders': {
+                'useDefault': false,
+                'overrides': [
+                    {'method': 'popup', 'minutes': 10}
+                ]
+            }
+        };
+        return JSON.stringify(event);
+    };
+
     var createEvent = function(eventData, callback){
-        _identity.authenticatedRequest('GET',
-            _CALENDAR_LIST_URI, true, function(error, status, response){
-                var response = response;
-                var items = response.items;
-            })
-    }
+        _identity.authenticatedRequest('POST', _CALENDAR_EVENT_URI, _createEventData(), true, function(error, status, response){
+            callback(response)
+        });
+    };
 
     return {
         init: init,
